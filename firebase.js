a1a4bfd8a1cc13b052c29370c632f4c23b5d4e54
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-09 22:52:27 +0400

    TravelMore project start

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcaK4eNPnV-dLGO1gOq76C0A_clg1ie4M",
+  authDomain: "travelmore-e65d4.firebaseapp.com",
+  databaseURL: "https://travelmore-e65d4-default-rtdb.firebaseio.com",
+  projectId: "travelmore-e65d4",
+  storageBucket: "travelmore-e65d4.appspot.com",
+  messagingSenderId: "713002452960",
+  appId: "1:713002452960:web:333c7a77944bc5253d4d85",
+  measurementId: "G-F4LKV57Y58",
+};
+
+const app = initializeApp(firebaseConfig);
