8427f8490cd9d527120ee7f22a7c97ccb4c17119
lukasordia123 <99127921+lukasordia123@users.noreply.github.com>
2022-06-16 17:30:12 +0400

    ს

---
+++ create hotels.js
@@ -0,0 +1,10 @@
+const InputOne = document.querySelector(".inputone")
+const InputTwo = document.querySelector(".inputtwo")
+const INputThree = document.querySelector(".inputthree")
+const INputFour = document.querySelector(".inputfour")
+const InputFive = document.querySelector(".inputfive")
+const BTN = document.querySelector(".Btn")
+
+BTN.addEventListener("click", () => {
+    
+} )
