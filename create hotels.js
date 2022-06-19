e0b3774a4fb37313b2615efac852d39019e7d34e
lukasordia123 <99127921+lukasordia123@users.noreply.github.com>
2022-06-19 15:59:27 +0400

    create hotels.js

--- create hotels.js
+++ create hotels.js
@@ -6,19 +6,43 @@ const InputFive = document.querySelector(".inputfive")
 const BTN = document.querySelector(".Btn")
 BTN.addEventListener("click", () => {
     let inputoneValue = InputOne.value;
-    let inoutwtoValue = InputTwo.value;
-    let inoutthreeValue = INputThree.value;
-    let inoutfourValue = INputFour.value;
-    let inoutfiveValue = InputFive.value;
-    if(inputoneValue==="" || inoutwtoValue==="" || inoutthreeValue==="" || inoutfourValue==="" || inoutfiveValue=== "" ) {
+    let inputtwoValue = InputTwo.value;
+    let inputthreeValue = INputThree.value;
+    let inputfourValue = INputFour.value;
+    let inputfiveValue = InputFive.value;
+    if(inputoneValue==="" || inputtwoValue==="" || inputthreeValue==="" || inputfourValue==="" || inputfiveValue=== "" ) {
         return;
     }
-    let imgSrc =""
-    const reader = new FileReader();
-    reader.readAsDataURL(imgSrc.files[0]);
-    reader.onload = () => {
+    let imgSrc = "";
+    try {
+      const reader = new FileReader();
+      reader.readAsDataURL(image.files[0]);
+      reader.onload = () => {
         imgSrc = reader.result;
-        console.log(imgSrc) 
-    };
+        addElementInFirebase("Post/", {
+          title: titleValue,
+          text: textValue,
+          imgSrc: imgSrc,
+        //   uploadTime: new Date().toLocaleString(),
+        });
+      };
+    } catch (err) {
+      imgSrc =
+        "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-6.png";
+      addElementInFirebase("Card/", {
+         InputOne:inputoneValue,
+         InputTwo:inputtwoValue,
+         INputThree:inputthreeValue,
+         INputFour:inputfourValue,
+         InputFive:inputfiveValue,
+         uploadTime: new Date().toLocaleString(),
+      });
+    }
+    displayAlert("წარმატებული ოპერაცია", "პოსტი წარმატებით დაემატა", "success");
+    InputOne.value = "";
+    InputTwo.value = "";
+    setTimeout(() => {
+      location.href = "index.html";
+    }, 1500);
 } )
 
