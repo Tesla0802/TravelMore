dfec004f27ea2409b62b10a4252b545717e865a5
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-23 21:12:42 +0400

    half of dark & light mode

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 const Variability = document.querySelector(".Day-Night");
 const body = document.querySelector("body");
 
-// Variability.addEventListener("click", () => {
-//   body.style.backgroundImage = "url(./img/bgNigth.dda13b0508ea72b6b5f0.png)";
-// });
+Variability.addEventListener("click", () => {
+  body.style.backgroundImage = "url(./img/bgNigth.dda13b0508ea72b6b5f0.png)";
+});
 
 // $(Variability).click(function () {
 //   $(body).css(
