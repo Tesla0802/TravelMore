bd80e2709c3266cce06084680f4e6201e13c4590
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-25 12:59:08 +0400

    log in position start

---
+++ adminLogin.js
@@ -0,0 +1,8 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const profile = document.querySelector(".dropdown-menu");
+
+function adminLogin() {
+  if (email.value == "admin@gmail.com") {
+  }
+}
