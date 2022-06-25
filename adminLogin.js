77745933f623fcd3568dd637b39b9616e49aa607
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-25 13:33:46 +0400

    log in position start

--- adminLogin.js
+++
@@ -1,8 +0,0 @@
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const profile = document.querySelector(".dropdown-menu");
-
-function adminLogin() {
-  if (email.value == "admin@gmail.com") {
-  }
-}
