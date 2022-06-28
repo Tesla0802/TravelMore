b818bfb88bb9f28c6d9b04b13e6a13ed6eb05a92
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-28 16:59:56 +0400

    rise

--- home.js
+++ home.js
@@ -1,15 +1,15 @@
-const Variability = document.querySelector(".Day-Night");
-const body = document.querySelector("body");
-let logic = true;
+// const Variability = document.querySelector(".Day-Night");
+// const body = document.querySelector("body");
+// let logic = true;
 
-Variability.addEventListener("click", () => {
-  if (logic) {
-    body.style.backgroundImage = "url(./img/bgNigth.dda13b0508ea72b6b5f0.png)";
-  } else {
-    body.style.backgroundImage = "url(./img/bgDay.074ef37336b466c36b93.png)";
-  }
-  logic = !logic;
-});
+// Variability.addEventListener("click", () => {
+//   if (logic) {
+//     body.style.backgroundImage = "url(./img/bgNigth.dda13b0508ea72b6b5f0.png)";
+//   } else {
+//     body.style.backgroundImage = "url(./img/bgDay.074ef37336b466c36b93.png)";
+//   }
+//   logic = !logic;
+// });
 
 // $(Variability).click(function () {
 //   $(body).css(
