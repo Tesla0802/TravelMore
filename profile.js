4a94330836a0307cfa35a2c7f008ea206a7ab193
Nika <99128017+Tesla0802@users.noreply.github.com>
2022-06-28 18:02:11 +0400

    update

--- profile.js
+++ profile.js
@@ -1,12 +1,15 @@
+const img = document.querySelector("#photo");
+const file = document.querySelector("#file");
+const uploadebtn = document.querySelector(".uploadebtn");
+
 file.addEventListener("change", function () {
-    const chooseFile = this.files[0];
-    if (chooseFile) {
-      const reader = new FileReader();
-  
-      reader.addEventListener("load", function () {
-        img.setAttribute("src", reader.result);
-      });
-      reader.readAsDataURL(chooseFile);
-    }
-  });
-  
+  const chooseFile = this.files[0];
+  if (chooseFile) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+      img.setAttribute("src", reader.result);
+    });
+    reader.readAsDataURL(chooseFile);
+  }
+});
